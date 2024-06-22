b2f4f1e9d74879932a2f6c978d9640dc173b9b97
Vishal Singh <singhkvishal@gmail.com>
2024-06-22 17:29:53 +0530

    New Module for ParaParallerl Execution example

---
+++ src/test/java/Parallel_Execution/TestParallel.java
@@ -0,0 +1,20 @@
+package Parallel_Execution;
+
+import org.testng.annotations.Test;
+//Run using the command to execute the .xml file
+// "java org.testng.TestNG testng.xml"
+public class TestParallel {
+    @Test
+    public void method1() {
+        long id = Thread.currentThread().getId();
+        System.out.println("Inside method1()" + " Thread Name =" +id);
+        //Assert.assertEquals(message, messageUtil.printMessage());
+    }
+
+    @Test
+    public void method2() {
+        long     id = Thread.currentThread().getId();
+        System.out.println("Inside method2()" + "Thread Name =" + id);
+        //Assert.assertEquals(message, messageUtil.printMessage());
+    }
+}
